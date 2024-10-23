b9c666a2c35296d352c7c87559931a3229cf1f4c
ShawnCarlile <srcarlil@svsu.edu>
2024-10-23 14:18:20 -0400

    Add files via upload

--- calculatorScript.js
+++ calculatorScript.js
@@ -1,6 +1,8 @@
 let mathProblem = ""; //variable that holds current problem
 let history = [];
+let index = 0;
 //event listener taht listens for any click on the page
+
 document.getElementById("button-history").addEventListener("click", ()=> {
     let historyDisplay = document.getElementById("equation");
     if(history.length === 0)
@@ -18,23 +20,26 @@ document.addEventListener("click", (event) => {
     let itemThatWasClick = event.target
     //checks if item clicked is a button
     if (itemThatWasClick.matches("button")) {
-
+        
         //check to see if button has the data attribute of data-number
         //if data-number is not on this button, it will check for other data-attributes
         if (itemThatWasClick.dataset.number !== undefined) {
             mathProblem += itemThatWasClick.dataset.number
+            history[index] = mathProblem
         }
         else {
             if (itemThatWasClick.dataset.operation === "ce") {
                 //if the button ce was clicked, clear everything
                 //a return will stop execution of this function
 
+                history[index] = mathProblem
                 mathProblem = clearEverything();
                 return;
             }
             else if (itemThatWasClick.dataset.operation !== "=") {
 
                 mathProblem += itemThatWasClick.dataset.operation;
+                history[index] = mathProblem
             }
             else {
                 updateAnswer();
@@ -43,9 +48,11 @@ document.addEventListener("click", (event) => {
         }
 
         updateCurrentProblem(mathProblem);
-        randomColor();
+        document.button.style.backgroundColor = randomColor();
+        index++;
     }
 })
+
 
 
 
@@ -71,5 +78,5 @@ function randomColor() {
     let r = Math.floor(Math.random() * 255);
     let g = Math.floor(Math.random() * 255);
     let b = Math.floor(Math.random() * 255);
-    document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    return `rgb(${r}, ${g}, ${b})`;
 }
